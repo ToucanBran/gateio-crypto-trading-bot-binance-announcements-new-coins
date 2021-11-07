db2dcfe9e904bdff34ab50c1c9baffaec63d25f7
toucanbran <brandon.gomez89@gmail.com>
2021-11-07 11:51:51 -0800

    added cronjob deployment for the scraper and made sure it could work with rabbitmq

--- announcement_scraper.py
+++ announcement_scraper.py
@@ -1,19 +1,20 @@
-from logging import Logger
-from services import BinanceScraper, queue
+from logger import logger
+from services import BinanceScraper, CoinService, queue
 from load_config import *
 
 def main(configs):
-    scraper = BinanceScraper()
-    last_article_id = 72913
+    scraper = BinanceScraper(CoinService())
+    last_article_id = 0
     new_coins = scraper.get_latest_coins(last_article_id)
-    Logger.debug(f"New coins: {', '.join(new_coins)}")
+    logger.debug(f"New coins: {', '.join(new_coins)}")
+    #retry loop
     with queue(configs["queue"]) as q:
         for coin in new_coins:
-            Logger.info(f"Pushing {coin} to queue")
+            logger.info(f"Pushing {coin} to queue")
             q.push(coin)
 
 
 if __name__ == '__main__':
-    Logger.info("Scanning announcements...")
+    logger.info("Scanning announcements...")
     configs = load_config("config.yml")
     main(configs)
